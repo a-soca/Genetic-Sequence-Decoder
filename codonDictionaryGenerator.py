0f15925903c5e6f4f59e259b49921ab366590ceb
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-08-30 11:08:34 +0200

    Add files via upload

--- codonDictionaryGenerator.py
+++ codonDictionaryGenerator.py
@@ -1,4 +1,4 @@
-codons = { 1: "C",
+mRNABaseDict = { 1: "C",
 2: "A",
 3: "U",
 4: "G",
@@ -7,4 +7,4 @@ codons = { 1: "C",
 for i in range(4):
 	for j in range(4):
 		for k in range(4):
-			print("\"" + codons[i+1] + codons[j+1] + codons[k+1] + "\"" + ": ")
+			print("\"" + mRNABaseDict[i+1] + mRNABaseDict[j+1] + mRNABaseDict[k+1] + "\"" + ": ")
--- generationFunctions.py
+++ generationFunctions.py
@@ -1,6 +1,6 @@
 import random
 
-randCodonsDict = { 1: "C",
+baseDict = { 1: "C",
 2: "A",
 3: "T",
 4: "G"
@@ -9,5 +9,5 @@ randCodonsDict = { 1: "C",
 def DNASeqGenerator(length):
 	DNASeq = ""
 	for i in range(length):
-		DNASeq = DNASeq + randCodonsDict[random.randint(1,4)]
+		DNASeq = DNASeq + baseDict[random.randint(1,4)]
 	return DNASeq
